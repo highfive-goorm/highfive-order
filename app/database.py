549649268844a8affd15454570ebd53a9d60468c
igh197 <ingyuhan497@gmail.com>
2025-05-22 16:29:56 +0900

    fix (HG-111): order 최종 수정

--- app/database.py
+++ app/database.py
@@ -1,11 +1,10 @@
 import os
 
+from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 
-DB_USER = os.getenv("DB_USER")
-MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
-MONGO_URI = f"mongodb://{DB_USER}:{MONGO_PASSWORD}@mongodb_order:27017/"
+MONGO_URI = f"mongodb://postgres:han00719@mongodb_order:27017/"
 client = AsyncIOMotorClient(MONGO_URI)
 db = client['order']
 
-order_collection = db['order']
+collection = db['order']
--- app/schemas.py
+++ app/schemas.py
@@ -7,24 +7,21 @@ from datetime import datetime
 class OrderItem(BaseModel):
     product_id: Optional[int]
     quantity: Optional[int]
-    price: Optional[int]
+    discounted_price: Optional[int]
 
 
 class OrderBase(BaseModel):
-    id: Optional[str] = None
+    id: Optional[int]
     user_id: Optional[str]
     status: Literal["paid", "shipping", "shipped", "cancelled"]
     order_items: List[OrderItem]
     total_price: Optional[int]
 
 
-class OrderCreate(OrderBase):
-    pass
 
 
 class OrderInDB(OrderBase):
     created_at: datetime
     updated_at: datetime
-
     class Config:
         orm_mode = True
