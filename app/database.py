7b2ea45a73672a5cde4654253a222d2a065642b7
igh197 <ingyuhan497@gmail.com>
2025-05-22 15:38:23 +0900

    fix (HG-111): mongodb_order 수정

--- app/database.py
+++ app/database.py
@@ -1,6 +1,10 @@
+import os
+
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URI = "mongodb://root:mongodb_order@mongodb_order:27017"
+DB_USER = os.getenv("DB_USER")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+MONGO_URI = f"mongodb://{DB_USER}:{MONGO_PASSWORD}//@mongodb_order:27017"
 client = AsyncIOMotorClient(MONGO_URI)
 db = client['order']
 
