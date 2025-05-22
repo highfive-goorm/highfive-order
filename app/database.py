f652aec86ca42e0800af77832023ab9cdd12fdb7
igh197 <ingyuhan497@gmail.com>
2025-05-22 15:43:03 +0900

    fix (HG-111): order db오타 수정

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 DB_USER = os.getenv("DB_USER")
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
-MONGO_URI = f"mongodb://{DB_USER}:{MONGO_PASSWORD}//@mongodb_order:27017"
+MONGO_URI = f"mongodb://{DB_USER}:{MONGO_PASSWORD}@mongodb_order:27017"
 client = AsyncIOMotorClient(MONGO_URI)
 db = client['order']
 
