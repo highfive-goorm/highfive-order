36dfd88729a47c6d141974825f774074e1e3133b
igh197 <ingyuhan497@gmail.com>
2025-05-23 15:16:08 +0900

    fix (HG-113): mvp 최종

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URI = f"mongodb://postgres:han00719@mongodb_order:27017/"
+MONGO_URI = f"mongodb://postgres:han00719@mongodb_order:27017"
 client = AsyncIOMotorClient(MONGO_URI)
 db = client['order']
 
--- app/schemas.py
+++ app/schemas.py
@@ -1,27 +1,35 @@
 # oder/app/schemas.py
 from typing import List, Literal, Optional
+from zoneinfo import ZoneInfo
+
 from pydantic import BaseModel, ConfigDict
 from datetime import datetime
 
 
 class OrderItem(BaseModel):
     product_id: Optional[int]
-    quantity: Optional[int]
-    discounted_price: Optional[int]
+    quantity: int
+    discounted_price: Optional[int] = None
+    discount: Optional[int] = None
+    img_url: Optional[str] = None
+    name: Optional[str] = None
+    price: Optional[int] = None
 
 
+    class Config:
+        orm_mode = True
 class OrderBase(BaseModel):
-    id: Optional[int]
     user_id: Optional[str]
     status: Literal["paid", "shipping", "shipped", "cancelled"]
     order_items: List[OrderItem]
+    is_from_cart: bool
     total_price: Optional[int]
 
 
 
 
 class OrderInDB(OrderBase):
-    created_at: datetime
-    updated_at: datetime
+    created_at: datetime=datetime.now(tz=ZoneInfo("Asia/Seoul"))
+    updated_at: datetime=datetime.now(tz=ZoneInfo("Asia/Seoul"))
     class Config:
         orm_mode = True
