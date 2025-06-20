f84e2e46cdad2c1308d5393f2ac863f1e30e0c2a
ksee1230 <ksee1230@gmail.com>
2025-06-20 15:35:43 +0900

    build, ci: 빌드 파일 수정, CI 설정 파일 수정

--- app/database.py
+++ app/database.py
@@ -4,12 +4,19 @@ from dotenv import load_dotenv, find_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 
 load_dotenv(find_dotenv(usecwd=True))
+
+user = os.getenv('MONGO_USER')
+password = os.getenv('MONGO_PASSWORD')
+hosts = os.getenv('MONGO_HOSTS')
+db_name = os.getenv('MONGO_DB')
+replica_set = os.getenv('MONGO_REPLICA_SET')
+
 MONGO_URI = (
-    f"mongodb://{os.getenv('DB_USER')}:{os.getenv('MONGO_PASSWORD')}@{os.getenv('MONGO_URL')}:{os.getenv('MONGO_PORT')}/{os.getenv('MONGO_DB')}?authSource=admin"
+    f"mongodb://{user}:{password}@{hosts}/{db_name}?authSource=admin&replicaSet={replica_set}"
 )
 
 client = AsyncIOMotorClient(MONGO_URI)
 
-db = client["product"]
+db = client[db_name]
 
 collection = db["order"]
